5cc301698499f2225b3b1108acf8096ce79e5360
Peter <peterphongpak@gmail.com>
2021-05-24 23:45:45 +0700

    Fix sometext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -265,7 +265,7 @@ const Index = () => {
                     id="password"
                     onChange={event => {setPassword(event.target.value)}}
                     className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                    placeholder="Password (For the first time type any)"
+                    placeholder="Password (For the first time, type anything)"
                     required
                   />
                 </div>
